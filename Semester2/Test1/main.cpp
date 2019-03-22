cf633aab24ed0dd87ca70773d14158eca6bc585d
Yurii <goldenapplemc3@gmail.com>
2019-03-23 00:30:04 +0200

    Finished test

--- Semester2/Test1/main.cpp
+++ Semester2/Test1/main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <utility>
+#include <ctime>
 
 const int SUB_VARIANT = 1;
 
@@ -87,17 +88,17 @@ std::ostream& operator<<(std::ostream& os, const Triangle& triangle)
 
 //T must be printable to cout
 template<typename T>
-struct LinkedNode
+struct DoubleNode
 {
     T value;
-    LinkedNode<T>* next;
-    LinkedNode<T>* prev;
+    DoubleNode<T>* next;
+    DoubleNode<T>* prev;
 
-    LinkedNode(T value, LinkedNode<T>* next = nullptr, LinkedNode<T>* prev = nullptr);
+    DoubleNode(T value, DoubleNode<T>* next = nullptr, DoubleNode<T>* prev = nullptr);
 };
 
 template<typename T>
-LinkedNode<T>::LinkedNode(T value, LinkedNode<T>* next, LinkedNode<T>* prev)
+DoubleNode<T>::DoubleNode(T value, DoubleNode<T>* next, DoubleNode<T>* prev)
 : value(value), next(next), prev(prev)
 {}
 
@@ -105,19 +106,34 @@ LinkedNode<T>::LinkedNode(T value, LinkedNode<T>* next, LinkedNode<T>* prev)
 
 
 template<typename T>
-struct LinkedList
+struct DoubleLinkedList
 {
-    LinkedNode<T>* start = nullptr;
-    LinkedNode<T>* end = nullptr;
+    int count = 0;
+    DoubleNode<T>* start = nullptr;
+    DoubleNode<T>* end = nullptr;
+    ~DoubleLinkedList();
 
     void insert_at_end(const T& value);
+    void remove(DoubleNode<T>* node, bool delete_node);
     void print();
 };
 
 template<typename T>
-void LinkedList<T>::insert_at_end(const T& value)
+DoubleLinkedList<T>::~DoubleLinkedList()
 {
-    LinkedNode<T>* new_node = new LinkedNode<T>(value);
+    DoubleNode<T>* current_node = this->start;
+    while(current_node)
+    {
+        DoubleNode<T>* next_node = current_node->next;
+        delete current_node;
+        current_node = next_node;
+    }
+}
+
+template<typename T>
+void DoubleLinkedList<T>::insert_at_end(const T& value)
+{
+    DoubleNode<T>* new_node = new DoubleNode<T>(value);
 
     if(this->end == nullptr)
     {
@@ -128,13 +144,33 @@ void LinkedList<T>::insert_at_end(const T& value)
     {
         this->end->next = new_node;
         new_node->prev = this->end;
+        this->end = new_node;
     }
+    this->count++;
 }
 
 template<typename T>
-void LinkedList<T>::print()
+void DoubleLinkedList<T>::remove(DoubleNode<T>* node, bool delete_node)
 {
-    LinkedNode<T>* current_element = this->start;
+    if(node == this->start)
+        this->start = node->next;
+    else
+        node->prev->next = node->next;
+
+    if(node == this->end)
+        this->end = node->prev;
+    else
+        node->next->prev = node->prev;
+
+    if(delete_node)
+        delete node;
+    this->count--;
+}
+
+template<typename T>
+void DoubleLinkedList<T>::print()
+{
+    DoubleNode<T>* current_element = this->start;
     while(current_element)
     {
         std::cout << current_element->value << std::endl;
@@ -146,23 +182,37 @@ void LinkedList<T>::print()
 
 
 template<typename T>
-using LinkedKeyNode = LinkedNode<std::pair<int, T>>;
+using DoubleKeyNode = DoubleNode<std::pair<int, T>>;
 
 //T must implement int getHash() const;
 //T must implement static T make_random();
 template<typename T>
 struct CycleKeyList
 {
-    LinkedNode<std::pair<int, T>>* end = nullptr;
-    int count;
+    DoubleKeyNode<T>* end = nullptr;
+    int count = 0;
+    ~CycleKeyList();
 
     void insert_at_end(const T& value);
-    void print() const;
+    void remove(DoubleKeyNode<T>* node, bool delete_node);
 
-    void modify();
+    void print(bool only_keys = false) const;
 
     static int get_hash(const T& value);
 };
+
+template<typename T>
+CycleKeyList<T>::~CycleKeyList()
+{
+    DoubleKeyNode<T>* current_node = this->end->next;
+    while(current_node != this->end)
+    {
+        DoubleKeyNode<T>* next_node = current_node->next;
+        delete current_node;
+        current_node = next_node;
+    }
+    delete this->end;
+}
 
 template<typename T>
 int CycleKeyList<T>::get_hash(const T& value)
@@ -175,16 +225,16 @@ void CycleKeyList<T>::insert_at_end(const T& value)
 {
     if(this->end == nullptr)
     {
-        LinkedKeyNode<T>* new_node = new LinkedKeyNode<T>(std::pair<int, T>(CycleKeyList::get_hash(value), value));
+        DoubleKeyNode<T>* new_node = new DoubleKeyNode<T>(std::pair<int, T>(CycleKeyList::get_hash(value), value));
         new_node->next = new_node;
         new_node->prev = new_node;
         this->end = new_node;
     }
     else
     {
-        LinkedKeyNode<T>* start = this->end->next;
+        DoubleKeyNode<T>* start = this->end->next;
 
-        LinkedKeyNode<T>* new_node = new LinkedKeyNode<T>(std::pair<int, T>(CycleKeyList::get_hash(value), value), start, this->end);
+        DoubleKeyNode<T>* new_node = new DoubleKeyNode<T>(std::pair<int, T>(CycleKeyList::get_hash(value), value), start, this->end);
 
         start->prev = new_node;
         this->end->next = new_node;
@@ -195,7 +245,28 @@ void CycleKeyList<T>::insert_at_end(const T& value)
 }
 
 template<typename T>
-void CycleKeyList<T>::print() const
+void CycleKeyList<T>::remove(DoubleKeyNode<T>* node, bool delete_node)
+{
+    if(node->next == node)
+    {
+        if(delete_node)
+            delete node;
+        this->end = nullptr;
+    }
+    else
+    {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        if(this->end == node)
+            this->end = node->next;
+        if(delete_node)
+            delete node;
+    }
+    count--;
+}
+
+template<typename T>
+void CycleKeyList<T>::print(bool only_keys) const
 {
     if(this->end == nullptr)
     {
@@ -204,92 +275,422 @@ void CycleKeyList<T>::print() const
     else
     {
         std::cout << this->count << " elements:" << std::endl;
-        LinkedKeyNode<T>* current_node = this->end;
+        DoubleKeyNode<T>* current_node = this->end;
         do
         {
-            std::cout << current_node->value.second << " (key = " << current_node->value.first << ")" << std::endl;
+            if(only_keys)
+            {
+                std::cout << current_node->value.first << " ";
+            }
+            else
+            {
+                std::cout << current_node->value.second << " (key = " << current_node->value.first << ")" << std::endl;
+            }
             current_node = current_node->next;
         }
         while(current_node != this->end);
+        if(only_keys)
+            std::cout << std::endl;
     }
 }
 
+template<typename T>
+bool valid_sequence_neighbours(const DoubleKeyNode<T>& node, const DoubleKeyNode<T>& next_node)
+{
+    return std::abs(node.value.first - next_node.value.first) <= 1;
+}
 
 template<typename T>
-void CycleKeyList<T>::modify()
+void task2(CycleKeyList<T>& list)
 {
     const int TOTAL_DELETED_MAX = 1327;
     int total_deleted = 0;
 
-    LinkedKeyNode<T>* sequence_start = this->end; //nullptr means we're not inside a sequence right now
-    LinkedKeyNode<T>* sequence_end = this->end; //start and end are inclusive
-    int sequence_length = 1;
+    //skip first sequence on the first run
+    bool first_sequence = true;
+
+    DoubleKeyNode<T>* current_node = list.end;
+    DoubleKeyNode<T>* last_sequence_start = current_node;
 
     while(true)
     {
-        bool deleted_this_cycle = false;
-        if(sequence_start != nullptr)
+        if(current_node->next == last_sequence_start)
         {
-            if(std::abs(sequence_end->next->value.first - sequence_end->value.first) <= 1)
-            {
-                sequence_end = sequence_end->next;
-                sequence_length++;
-            }
-            else //sequence is done, replacing/deleting
-            {
-                LinkedKeyNode<T>* insert_start_node = sequence_start->prev;
-                LinkedKeyNode<T>* insert_end_node = sequence_end->next;
-
-                //deleting
-                LinkedKeyNode<T>* current_node = sequence_start;
-                while(current_node != sequence_end)
-                {
-                    LinkedKeyNode<T>* next_node = current_node->next;
-                    delete current_node;
-                    current_node = next_node;
-                }
-                delete sequence_end;
-
-                total_deleted = sequence_length;
-
-                //adding new data
-                LinkedKeyNode<T>* new_node;
-                for(int i = 0; i < sequence_length / 3; i++)
-                {
-                    T value = T::make_random();
-                    new_node = new LinkedKeyNode<T>(std::pair<int, T>(CycleKeyList::get_hash(value), value));
-
-                    insert_start_node->next = new_node;
-                    new_node->prev = insert_start_node;
-                }
-                insert_end_node->prev = new_node;
-            }
+            //We just looped through the entire list
+            //This means there are no more sequences left
+            std::cout << "No more sequences left; return." << std::endl;
+            return;
         }
+
+        if(valid_sequence_neighbours(*current_node, *current_node->next))
+        {
+            if(first_sequence)
+            {
+                current_node = current_node->next;
+                continue;
+            }
+
+            //we're in a sequence; delete next element until sequence is over
+            list.print(true);
+
+            last_sequence_start = current_node->prev;
+            int sequence_length = 2;
+            //temporary storage to compare to the element after it
+            DoubleKeyNode<T>* current_sequence_end = current_node->next;
+            list.count--;
+            while(true)
+            {
+                //remove current_sequence_end but not delete
+                list.remove(current_sequence_end, false);
+                /*current_node->next = current_sequence_end->next;
+                current_sequence_end->next->prev = current_node;*/
+                if(current_node->next == current_node) //infinite loop
+                {
+                    std::cout << "List is an infinite loop; return." << std::endl;
+                    return;
+                }
+                if(valid_sequence_neighbours(*current_sequence_end, *current_node->next))
+                {
+                    delete current_sequence_end;
+                    current_sequence_end = current_node->next;
+                    sequence_length++;
+                    list.count--;
+                    continue;
+                }
+                break;
+            }
+            delete current_sequence_end;
+            //delete current node (which is the sequence start)
+            list.remove(current_node, true);
+            /*current_node->prev->next = current_node->next;
+            current_node->next->prev = current_node->prev;
+            delete current_node;
+            this->count--;*/
+            std::cout << "Deleted " << sequence_length << " elements." << std::endl;
+            list.print(true);
+
+            total_deleted += sequence_length;
+
+            current_node = last_sequence_start;
+            for(int i = 0; i < sequence_length / 3; i++)
+            {
+                T value = T::make_random();
+                DoubleKeyNode<T>* new_node =
+                        new DoubleKeyNode<T>(std::pair<int, T>(CycleKeyList<T>::get_hash(value), value), current_node->next, current_node);
+                current_node->next->prev = new_node;
+                current_node->next = new_node;
+
+                current_node = current_node->next;
+                list.count++;
+            }
+            std::cout << "Inserted " << sequence_length / 3 << " elements. " << std::endl;
+            list.print(true);
+            std::cout << std::endl;
+            //std::cin.get();
+            if(total_deleted > TOTAL_DELETED_MAX)
+            {
+                std::cout << "Reached max deletions" << std::endl;
+                return;
+            }
+
+        }
+        else
+        {
+            first_sequence = false;
+        }
+        current_node = current_node->next;
     }
 }
 
+
+
+
+
+template<typename T>
+struct Node
+{
+    T value;
+    Node<T>* next;
+    Node(T value, Node<T>* next = nullptr);
+};
+
+template<typename T>
+Node<T>::Node(T value, Node<T>* next)
+: value(value), next(next)
+{}
+
+
+
+
+template<typename T>
+struct Stack
+{
+private:
+    Node<T>* top = nullptr;
+
+public:
+    void push(const T& value);
+    T pop();
+    void print();
+    ~Stack();
+};
+
+template<typename T>
+Stack<T>::~Stack()
+{
+    Node<T>* current_node = this->top;
+    while(current_node)
+    {
+        Node<T>* next_node = this->top->next;
+        delete current_node;
+        current_node = current_node->next;
+    }
+}
+
+template<typename T>
+void Stack<T>::push(const T& value)
+{
+    Node<T>* new_node = new Node<T>(value, this->top);
+    this->top = new_node;
+}
+
+template<typename T>
+T Stack<T>::pop()
+{
+    T value = this->top->value;
+    Node<T>* next_node = this->top->next;
+    delete this->top;
+    this->top = next_node;
+    return value;
+}
+
+template<typename T>
+void Stack<T>::print()
+{
+    Node<T>* current_node = this->top;
+    while(current_node != nullptr)
+    {
+        std::cout << current_node->value << std::endl;
+        current_node = current_node->next;
+    }
+}
+
+
+
+template<typename T>
+void task3(DoubleLinkedList<T>& list)
+{
+    Stack<T> stack;
+
+    int step_1_count = list.count / 4;
+    for(int i = 0; i < step_1_count; i++)
+    {
+        T value = list.start->value;
+        list.remove(list.start, true);
+        stack.push(value);
+    }
+    std::cout << "List: " << std::endl;
+    list.print();
+    std::cout << std::endl;
+    std::cout << "Stack: " << std::endl;
+    stack.print();
+    std::cout << std::endl;
+
+    DoubleNode<T>* current_node = list.start;
+    int steps = 0;
+    while(current_node != nullptr)
+    {
+        T value1 = current_node->value;
+        list.remove(current_node, false);
+        T value2 = stack.pop();
+        T value3 = T::make_random();
+        int hashes[3] = { value1.get_hash() % 4, value2.get_hash() % 4, value3.get_hash() % 4 };
+
+        std::cout << "List element: " << value1 << std::endl;
+        std::cout << "Stack element: " << value2 << std::endl;
+        std::cout << "Random element: " << value3 << std::endl;
+
+        //quick & dirty check
+        T push_value = value1;
+        if(hashes[0] == hashes[1] && hashes[1] != hashes[2])
+            push_value = value3;
+        else if(hashes[0] == hashes[2] && hashes[1] != hashes[2])
+            push_value = value2;
+        else
+            push_value = T::make_random();
+
+        stack.push(push_value);
+
+        std::cout << "Pushed element: " << push_value << std::endl << std::endl;
+        steps++;
+        if(steps % 10 == 0)
+        {
+            std::cout << "List: " << std::endl;
+            list.print();
+            std::cout << std::endl;
+            std::cout << "Stack: " << std::endl;
+            stack.print();
+            std::cout << std::endl;
+        }
+
+
+        DoubleNode<T>* next_node = current_node->next;
+        delete current_node;
+        current_node = next_node;
+    }
+
+    std::cout << "Stack: " << std::endl;
+    stack.print();
+    std::cout << std::endl;
+}
+
+
+
+
+template<typename T>
+struct LinkedList
+{
+    Node<T>* start = nullptr;
+    Node<T>* end = nullptr;
+    void insert_at_end(const T& value);
+    void print() const;
+
+    ~LinkedList();
+};
+
+template<typename T>
+LinkedList<T>::~LinkedList()
+{
+    Node<T>* current_node = this->start;
+    while(current_node)
+    {
+        Node<T>* next_node = current_node->next;
+        delete current_node;
+        current_node = next_node;
+    }
+
+}
+
+template<typename T>
+void LinkedList<T>::insert_at_end(const T& value)
+{
+    Node<T>* new_node = new Node<T>(value);
+
+    if(this->end == nullptr)
+    {
+        this->end = new_node;
+        this->start = new_node;
+    }
+    else
+    {
+        this->end->next = new_node;
+        this->end = new_node;
+    }
+}
+
+template<typename T>
+void LinkedList<T>::print() const
+{
+    Node<T>* current_node = this->start;
+    while(current_node)
+    {
+        std::cout << current_node->value << std::endl;
+        current_node = current_node->next;
+    }
+}
+
+
+
+template<typename T>
+struct HashTable
+{
+private:
+    const static int BUCKETS_COUNT = 101;
+    LinkedList<std::pair<int, T>> buckets[BUCKETS_COUNT];
+
+public:
+    void add(const T& value);
+    LinkedList<T> find_values(int hash) const;
+};
+
+template<typename T>
+void HashTable<T>::add(const T& value)
+{
+    int hash = value.get_hash();
+    this->buckets[hash % BUCKETS_COUNT].insert_at_end(std::pair<int, T>(hash, value));
+}
+
+template<typename T>
+LinkedList<T> HashTable<T>::find_values(int hash) const
+{
+    LinkedList<T> result_list;
+
+    Node<std::pair<int, T>>* current_node = buckets[hash % BUCKETS_COUNT].start;
+    while(current_node)
+    {
+        if(current_node->value.first == hash)
+            result_list.insert_at_end(current_node->value.second);
+        current_node = current_node->next;
+    }
+    return result_list;
+}
+
+
 int main()
 {
+    std::srand(std::time(nullptr));
+
     //TASK 1
-    LinkedList<Triangle> list1;
+
+    DoubleLinkedList<Triangle> list1;
 
     list1.insert_at_end(Triangle::make_random());
     list1.insert_at_end(Triangle::make_random());
 
     list1.print();
 
+    std::cin.get();
+
 
     //TASK 2
+
     std::cout << std::endl;
 
     CycleKeyList<Triangle> list2;
-    list2.insert_at_end(Triangle::make_random());
-    list2.insert_at_end(Triangle::make_random());
-    list2.insert_at_end(Triangle::make_random());
-    list2.insert_at_end(Triangle::make_random());
-    list2.print();
+    for(int i = 0; i < 10000; i++)
+    {
+        list2.insert_at_end(Triangle::make_random());
+    }
 
-    //list2.modify(); //DOES NOT WORK, has no end condition
+    task2(list2);
+
+    std::cin.get();
+
+
+    //TASK 3
+
+    DoubleLinkedList<Triangle> list3;
+    for(int i = 0; i < 100; i++)
+    {
+        list3.insert_at_end(Triangle::make_random());
+    }
+    list3.print();
+    task3(list3);
+
+    std::cin.get();
+
+
+
+    //TASK 4
+    HashTable<Triangle> list4;
+    for(int i = 0; i < 100000; i++)
+    {
+        list4.add(Triangle::make_random());
+    }
+    std::cout << "Search result: " << std::endl;
+    list4.find_values(2000).print();
+
+    std::cin.get();
 
     return 0;
 }
