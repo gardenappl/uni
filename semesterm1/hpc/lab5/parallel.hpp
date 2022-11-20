9dc28d5c9d52b148a64d1fa61496a8303fef8ee4
gardenapple <mailbox@appl.garden>
2022-11-21 01:26:03 +0200

    hpc: Lab 5 done + minor change to lab 4 results

--- semesterm1/hpc/lab5/parallel.hpp
+++ semesterm1/hpc/lab5/parallel.hpp
@@ -8,6 +8,7 @@ void DummyDataInitialization(int *pMatrix, int Size);
 void RandomDataInitialization(int *pMatrix, int Size);
 void DataDistribution(int *pMatrix, int *pProcRows, int Size, int RowNum);
 void ResultCollection(int *pMatrix, int *pProcRows, int Size, int RowNum);
+void ParallelFloyd(int *pProcRows, int Size, int RowNum);
 void RowDistribution(int *pProcRows, int Size, int RowNum, int k, int *pRow);
 void ParallelPrintMatrix(int *pProcRows, int Size, int RowNum);
 void TestDistribution(int *pMatrix, int *pProcRows, int Size, int RowNum);
